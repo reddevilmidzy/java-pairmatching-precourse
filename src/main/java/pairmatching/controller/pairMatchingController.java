27dfb1da572959fc3218cdaf75932218e7bbeb8e
은지 <midzy0228@gmail.com>
2022-12-15 18:23:54 +0900

    feat: 컨트롤러 생성

--- src/main/java/pairmatching/controller/pairMatchingController.java
+++ src/main/java/pairmatching/controller/pairMatchingController.java
@@ -1,2 +1,21 @@
-package pairmatching.controller;public class pairmatchingController {
+package pairmatching.controller;
+
+import pairmatching.model.Feature;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+public class pairMatchingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public pairMatchingController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        String input = inputView.chooseFeature();
+        Feature feature = new Feature(input);
+
+    }
 }
